2cde62e33ec24cac37a15c592040dcf029cdbce6
shahgee77 <hashmigee96@gmail.com>
2023-01-10 16:27:28 +0500

    First Commit

---
+++ src/components/FeedbackData.jsx
@@ -0,0 +1,26 @@
+import { FaTimes, FaEdit } from "react-icons/fa";
+import Card from "./shared/Card";
+import { useContext } from "react";
+import PropTypes from "prop-types";
+import FeedbackContext from "../context/FeedbackContext";
+
+export default function Feedbackitem({ item }) {
+  const { deleteFeedback, editFeedback } = useContext(FeedbackContext);
+  return (
+    <Card>
+      <div className="num-display">{item.rating}</div>
+      <button className="close" onClick={() => deleteFeedback(item.id)}>
+        <FaTimes color="purple" />
+      </button>
+      <button className="edit" onClick={() => editFeedback(item)}>
+        <FaEdit color="purple" />
+      </button>
+
+      <div className="text-display">{item.text}</div>
+    </Card>
+  );
+}
+
+Feedbackitem.propTypes = {
+  item: PropTypes.object.isRequired,
+};
---
+++ src/components/FeedbackList.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useContext } from "react";
+import FeedbackContext from "../context/FeedbackContext";
+import Spinner from "./shared/Spinner";
+import FeedbackData from "./FeedbackData";
+
+function FeedbackList() {
+  const { feedback, isLoading } = useContext(FeedbackContext);
+  if (!isLoading && (!feedback || feedback.length === 0)) {
+    return <p>No Feedback Yet</p>;
+  }
+  return isLoading ? (
+    <Spinner />
+  ) : (
+    <div className="feedback-list">
+      {feedback.map((item) => (
+        <FeedbackData key={item.id} item={item} />
+      ))}
+    </div>
+  );
+}
+
+export default FeedbackList;
